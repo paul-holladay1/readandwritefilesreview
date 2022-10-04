4c2307fec19e8e4a5be53728d2d14fcaa82a2706
Paul <pholladay@test.com>
2022-10-03 20:13:50 -0500

    Almost done

---
+++ add_my_name.py
@@ -0,0 +1,10 @@
+def main():
+
+    outfile = open('philosophers.txt', 'a')
+
+    outfile.write('Paul Holladay\n')
+
+    outfile.close()
+
+main()
+
---
+++ print_customers.py
@@ -0,0 +1,17 @@
+import csv
+
+def main():
+    infile = open('customers.csv', 'r')
+
+    csvfile = csv.reader(infile, delimiter = ',')
+
+    for record in csvfile:
+        print('ID: ', record[0])
+        print('First Name: ', record[1])
+        print('Last Name: ', record[2])
+        print('City: ', record[3])
+        print('Country: ', record[4])
+        print('Phone: ', record[4])
+
+
+main()
